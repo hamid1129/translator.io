10d88ce6abf3d8720da556b4d7d54eb3660db29a
Hamid Samani <hamid1129@gmail.com>
2014-02-13 11:16:09 +0330

    Added database integration tests

--- translate.io/src/test/java/info/translate/controller/UserControllerTest.java
+++ translate.io/src/test/java/info/translate/controller/UserControllerTest.java
@@ -1,6 +1,36 @@
 package info.translate.controller;
 
+import static org.junit.Assert.*;
+import info.translate.configuration.DataJPAConfiguration;
+import info.translate.domain.User;
+import info.translate.repository.UserRepository;
 
-public class UserControllerTest {
+import org.junit.Test;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(classes = DataJPAConfiguration.class)
+public class UserControllerTest extends AbstractBaseConfig {
+	@Autowired
+	UserRepository repository;
+
+	@Test
+	public void insertUser() {
+		User user = new User();
+		user.setUsername("hamid");
+
+		User user2 = repository.save(user);
+		assertEquals(user2.getId(), repository.findOne(user2.getId()).getId());
+
+	}
+
+	@Test
+	public void hashedPassword() {
+		User user = new User();
+		user.setUsername("hamid");
+
+		user.setPassword(BCrypt.hashpw("123456", BCrypt.gensalt()));
+		assertTrue(BCrypt.checkpw("123456", user.getPassword()));
+	}
 }
